071206a215f35d52de2698c7f4135f84eef5048a
evomimic <evomimic@gmail.com>
2023-11-12 16:57:34 -0700

    restructure complete, tryorama tests passed, but holon definition is sparse

--- crates/shared_types/holon/src/holon.rs
+++ crates/shared_types/holon/src/holon.rs
@@ -1,6 +1,19 @@
 use hdi::prelude::*;
+//use holon::Holon;
+use crate::object_ref::HolonReference;
+
 #[hdk_entry_helper]
 #[derive(Clone, PartialEq)]
 pub struct Holon {
-    pub descriptor: ActionHash,
+    pub descriptor: HolonReference,
+
+}
+
+#[hdk_entry_helper]
+#[derive(Clone, PartialEq)]
+pub struct HolonCollection {
+    pub item_type: HolonReference, // HolonDescriptor
+    pub items : Vec<HolonReference>,
+
+
 }
