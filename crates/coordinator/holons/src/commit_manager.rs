2141f21fdd666008c9e1368d2854abbd5176457e
Zeek <zkringlwe@gmail.com>
2024-02-13 13:48:43 -0700

    revert

--- crates/coordinator/holons/src/commit_manager.rs
+++ crates/coordinator/holons/src/commit_manager.rs
@@ -7,22 +7,6 @@ use crate::holon_errors::HolonError;
 use crate::holon_types::Holon;
 use shared_types_holon::MapString;
 
-// use thiserror::Error;
-
-// #[hdk_entry_helper]
-// #[derive(Error, Eq, PartialEq)]
-// pub enum CommitManagerError {
-//     #[error("Error during commit: {0}")]
-//     CommitError(String),
-// }
-
-// pub struct HolonErrorCase {
-//     pub key: String,
-//     pub holon: Holon,
-//     // pub holon: Arc<Holon>,
-//     pub error: HolonError,
-// }
-
 #[derive(Debug, Eq, PartialEq)]
 pub struct CommitResponse {
     pub status: CommitRequestStatusCode,
@@ -40,23 +24,23 @@ pub enum CommitRequestStatusCode {
 pub struct CommitError {
     pub holon_key: MapString,
     pub error_code: HolonError,
-    // pub description: MapString,
 }
 
 #[derive(Default, Clone, Debug)]
 pub struct CommitManager {
-    pub staged_holons: BTreeMap<String, Holon>,
+    pub staged_holons: BTreeMap<MapString, Holon>,
     // pub staged_holons: BTreeMap<String, Arc<Holon>>, // <Key, immutable reference>
 }
 
 impl CommitManager {
-    pub fn stage(&mut self, key: String, holon: Holon) {
+    pub fn stage(&mut self, key: MapString, holon: Holon) {
         self.staged_holons.insert(key, holon);
     }
 
-    pub fn get_by_key(&self, key: String) -> Option<&Holon> {
+    pub fn get_by_key(&self, key: MapString) -> Option<&Holon> {
         self.staged_holons.get(&key)
     }
+    /*
     // pub fn get_by_key(&self, key: String) -> Option<Arc<Holon>> {
     //     let fetch_option = self.staged_holons.get(&key);
     //     if let Some(holon) = fetch_option {
@@ -64,6 +48,7 @@ impl CommitManager {
     //     }
     //     else { None }
     // }
+     */
 
     // pub fn commit(&mut self) -> Result<Vec<HolonErrorCase>, CommitManagerError> {
     pub fn commit(&mut self) -> CommitResponse {
@@ -76,75 +61,27 @@ impl CommitManager {
                 }
                 Err(e) => {
                     let commit_error = CommitError {
-                        holon_key: MapString(k.to_string()),
+                        holon_key: k.clone(),
                         error_code: e,
-                        // description: MapString("".to_string()),
                     };
                     errors.push(commit_error);
                 }
             }
         }
         let error_count = errors.len();
-        if errors.is_empty() {
-            let commit_response = CommitResponse {
+        let commit_response = if errors.is_empty() {
+            CommitResponse {
                 status: CommitRequestStatusCode::Success,
                 description: MapString("All holons successfully committed".to_string()),
                 errors: None,
-            };
-            return commit_response;
+            }
         } else {
-            let commit_response = CommitResponse {
-                status: CommitRequestStatusCode::Success,
+            CommitResponse {
+                status: CommitRequestStatusCode::Error,
                 description: MapString(format!("Error committing {:?} holons", error_count)),
                 errors: Some(errors),
-            };
-            return commit_response;
+            }
         };
+        commit_response
     }
 }
-
-// let mut commit_count = 0;
-// let mut errors: Vec<HolonErrorCase> = Vec::new();
-// /*  Arc:
-// // for (k, v) in self.staged_holons.iter() {
-// //     let holon = v.as_ref();
-// //     let result = holon.commit();
-// //     match result {
-// //         Ok(holon) => {
-// //             self.staged_holons.insert(k.to_string(), Arc::new(holon));
-// //             commit_count += 1;
-// //         }
-// //         Err(e) => {
-// //             let holon_error_case = HolonErrorCase {
-// //                 key: k.to_string(),
-// //                 holon: *v,
-// //                 error: e,
-// //             };
-// //             errors.push(holon_error_case);
-// //         }
-// //     }
-// */
-// for (k, v) in self.clone().staged_holons.iter() {
-//     let result = v.clone().commit();
-//     match result {
-//         Ok(holon) => {
-//             self.staged_holons.insert(k.to_string(), holon);
-//             commit_count += 1;
-//         }
-//         Err(e) => {
-//             let holon_error_case = HolonErrorCase {
-//                 key: k.to_string(),
-//                 holon: v.clone(),
-//                 error: e,
-//             };
-//             errors.push(holon_error_case);
-//         }
-//     }
-// }
-// if commit_count == 0 {
-//     return Err(CommitManagerError::CommitError(
-//         "all holons failed to commit".to_string(),
-//     ));
-// } else {
-//     return Ok(errors);
-// }
