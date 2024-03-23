eb47982d9a8dad031030aeb2a2ef005e53753f6a
owleyeview <rashaanlightpool@gmail.com>
2024-03-22 18:59:53 -0700

    changing ValidationError into an enum, in process

--- crates/shared_types/holon/src/validation_error.rs
+++ crates/shared_types/holon/src/validation_error.rs
@@ -1,21 +1,41 @@
+use hdk::prelude::WasmError; // Ensure you have this import for WasmError
 use serde::{Deserialize, Serialize};
 use std::fmt;
+use thiserror::Error;
 
-#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
-pub struct ValidationError {
-    pub message: String,
+#[derive(Error, Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+pub enum ValidationError {
+    #[error("Property error: {0}")]
+    PropertyError(String),
+
+    #[error("Relationship error: {0}")]
+    RelationshipError(String),
+
+    #[error("Descriptor error: {0}")]
+    DescriptorError(String),
+
+    #[error("Wasm error: {0}")]
+    WasmError(String),
 }
 
-impl ValidationError {
-    pub fn new(message: &str) -> Self {
-        Self { message: message.to_string() }
-    }
-}
-
-// Implementing Display for ValidationError
 impl fmt::Display for ValidationError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}", self.message)
+        write!(f, "{}", self)
     }
 }
 
+impl From<WasmError> for ValidationError {
+    fn from(error: WasmError) -> Self {
+        ValidationError::WasmError(error.to_string())
+    }
+}
+
+impl Into<WasmError> for ValidationError {
+    fn into(self) -> WasmError {
+        match self {
+            ValidationError::WasmError(msg) => wasm_error!(msg),
+            // Handle other variants if necessary, converting them to a WasmError representation
+            _ => wasm_error!("Unhandled ValidationError type"),
+        }
+    }
+}
