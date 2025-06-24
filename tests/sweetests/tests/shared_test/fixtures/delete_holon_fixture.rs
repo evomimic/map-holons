40d74faa3502c124bf4bb3f3db308cea7840c37a
Zeek <zkringlwe@gmail.com>
2025-06-23 20:52:49 -0400

    add context to delete_holon_fixture

--- tests/sweetests/tests/shared_test/fixtures/delete_holon_fixture.rs
+++ tests/sweetests/tests/shared_test/fixtures/delete_holon_fixture.rs
@@ -2,20 +2,25 @@
 
 use rstest::*;
 
-use crate::shared_test::test_data_types::DancesTestCase;
+use crate::shared_test::{
+    test_context::{init_test_context, TestContextConfigOption::TestFixture},
+    test_data_types::{DancesTestCase, BOOK_KEY},
+};
 use base_types::{BaseValue, MapBoolean, MapInteger, MapString};
 use core_types::HolonId;
 use holons_core::{
     core_shared_objects::holon::{Holon, TransientHolon},
     dances::dance_response::ResponseStatusCode,
     query_layer::QueryExpression,
-    HolonCollection, HolonError, HolonsContextBehavior, RelationshipName,
+    stage_new_holon_api, HolonCollection, HolonError, HolonsContextBehavior, RelationshipName,
 };
 use integrity_core_types::{PropertyMap, PropertyName, PropertyValue};
 
 /// Fixture for creating a DeleteHolon Testcase
 #[fixture]
 pub fn delete_holon_fixture() -> Result<DancesTestCase, HolonError> {
+    let fixture_context = init_test_context(TestFixture);
+
     let mut test_case = DancesTestCase::new(
         "DeleteHolon Testcase".to_string(),
         "Tests delete_holon dance, matches expected response, in the OK case confirms get_holon_by_id returns NotFound error response for the given holon_to_delete ID.".to_string(),
@@ -23,23 +28,25 @@ pub fn delete_holon_fixture() -> Result<DancesTestCase, HolonError> {
 
     //  ADD STEP:  STAGE:  Book Holon  //
     let mut book_holon = TransientHolon::new();
-    let book_holon_key = MapString(
-        "Emerging World: The Evolution of Consciousness and the Future of Humanity".to_string(),
-    );
-    book_holon.with_property_value(
-        PropertyName(MapString("key".to_string())),
-        Some(BaseValue::StringValue(book_holon_key.clone())),
-    )?;
-    book_holon.with_property_value(
-        PropertyName(MapString("title".to_string())),
-        Some(BaseValue::StringValue(MapString(
-            "Emerging World: The Evolution of Consciousness and the Future of Humanity".to_string(),
-        )),
-    ))?.with_property_value(
-        PropertyName(MapString("description".to_string())),
-        Some(BaseValue::StringValue(MapString("Why is there so much chaos and suffering in the world today? Are we sliding towards dystopia and perhaps extinction, or is there hope for a better future?".to_string())),
-    ))?;
-    test_case.add_stage_holon_step(book_holon)?;
+    let book_holon_key = MapString(BOOK_KEY.to_string());
+
+    book_holon
+        .with_property_value(
+            PropertyName(MapString("key".to_string())),
+            Some(BaseValue::StringValue(book_holon_key.clone())),
+        )?
+        .with_property_value(
+            PropertyName(MapString("title".to_string())),
+            Some(BaseValue::StringValue(book_holon_key.clone())),
+        )?
+        .with_property_value(
+            PropertyName(MapString("description".to_string())),
+            Some(BaseValue::StringValue(MapString(
+                "Why is there so much chaos and suffering in the world today? Are we sliding towards dystopia and perhaps extinction, or is there hope for a better future?".to_string(),
+            ))),
+        )?;
+    test_case.add_stage_holon_step(book_holon.clone())?;
+    let book_ref = stage_new_holon_api(&*fixture_context, book_holon)?;
 
     // ADD STEP:  COMMIT  // all Holons in staging_area
     test_case.add_commit_step()?;
