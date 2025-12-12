9050d34c1c876540edc9dda2d906e6e0b3b8e82a
Zeek <zkringlwe@gmail.com>
2025-12-12 00:31:18 -0500

    ergonomic relationships passing

--- tests/sweetests/tests/helpers/constants.rs
+++ tests/sweetests/tests/helpers/constants.rs
@@ -7,6 +7,7 @@ pub const PERSON_1_KEY: &str = "Roger Briggs";
 pub const PERSON_2_KEY: &str = "George Smith";
 pub const PUBLISHER_KEY: &str = "Publishing Company";
 pub const BOOK_TO_PERSON_RELATIONSHIP: &str = "AUTHORED_BY";
+pub const PUBLISHED_BY: &str = "PUBLISHED_BY";
 pub const EDITOR_FOR: &str = "EDITOR_FOR";
 pub const BOOK_DESCRIPTOR_KEY: &str = "Book.HolonType";
 pub const PERSON_DESCRIPTOR_KEY: &str = "Person.HolonType";
