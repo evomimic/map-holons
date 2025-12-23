9af35dc6022e5001ff7b4060f288898c4bf85f61
Thomas <nphias@gmail.com>
2025-12-23 22:14:44 +0000

    ci fix tests

--- tests/tryorama/src/map_holons/holons/test-case-1.test.ts
+++ tests/tryorama/src/map_holons/holons/test-case-1.test.ts
@@ -11,7 +11,7 @@ test('TEST CASE 1, Stage, Add Properties, Commit Holons', async () => {
   await runScenario(async scenario => {
     // Construct proper paths for your app.
     // This assumes app bundle created by the `hc app pack` command.
-    const testAppPath = process.cwd() + '/../../happ/workdir/map-holons.happ';
+    const testAppPath = process.cwd() + '/../../workdir/map-holons.happ';
 
     // Set up the app to be installed 
     const appbundlesource: AppBundleSource = {
