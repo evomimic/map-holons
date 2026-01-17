de9a6eadfba712276d8966ba0e4effc0ca1e175a
evomimic <evomimic@gmail.com>
2026-01-17 06:03:00 -0700

    rebased from main to pickup PR 372 -- one fix -- npm test passes all tests

--- tests/sweetests/tests/execution_steps/new_holon_executor.rs
+++ tests/sweetests/tests/execution_steps/new_holon_executor.rs
@@ -23,7 +23,7 @@ pub async fn execute_new_holon(
     debug!("Dance Request: {:#?}", request);
 
     // 2. CALL - the dance
-    let dance_initiator = context.get_space_manager().get_dance_initiator().unwrap();
+    let dance_initiator = context.get_dance_initiator().unwrap();
     let response = dance_initiator.initiate_dance(context, request).await;
     info!("Dance Response: {:#?}", response.clone());
 
